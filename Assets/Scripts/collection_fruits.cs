cb6df1500450b79f53bca2aa9ed0fca40e625131
minimiGamesA <minimiGamesAM@GMAIL.COM>
2021-03-31 22:55:11 -0400

    add scrips rotation and collective

---
+++ Assets/Scripts/collection_fruits.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// test
+/// </summary>
+
+public class collection_fruits : MonoBehaviour
+{
+    private int gold;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "coin")
+        {
+            gold++;
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/rotation_coins.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// test
+/// </summary>
+
+public class rotation_coins : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
