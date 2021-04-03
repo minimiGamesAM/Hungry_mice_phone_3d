8ac293f235a585f3579014fb024f71d268f12ff6
minimiGamesA <minimiGamesAM@GMAIL.COM>
2021-04-02 20:08:04 -0400

    conver enemis to chikens

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -16,4 +16,16 @@ public class Projectile : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * 50);
         
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Instantiate(Resources.Load("collectable"), other.transform.position, Quaternion.Euler(90,0,0));
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+
+
+    }
 }
