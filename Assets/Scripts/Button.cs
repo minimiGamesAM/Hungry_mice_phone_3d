b29174855cf8c91232bdf856f5f81b7a9f60e78d
minimiGamesA <minimiGamesAM@GMAIL.COM>
2021-04-03 22:05:26 -0400

    add menus

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// test game
+/// </summary>
+
+public class Button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitAplication()
+    {
+        Application.Quit();
+    }
+
+}
